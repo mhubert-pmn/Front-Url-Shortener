6230080f5f791770f3d86ae6edd845e74c165636
CHRIS BAHONDA <gyde-feiry.chris-bahonda@capgemini.com>
2021-07-22 15:29:38 +0200

    Add a test button

--- front-url-shortener/src/components/SignUp/SignUp.js
+++ front-url-shortener/src/components/SignUp/SignUp.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../layout/Button/Button';
 
 const SignUp = () => {
   return(
     <div>
       <h1>S'inscrire</h1>
+      <button type="rounded"> Click here </button>
     </div>
   )
 }
