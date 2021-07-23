41a10ef720c067d783ff89186b1faf03eb42512a
Sibrider <sibrider@hotmail.fr>
2021-07-23 14:33:22 +0200

    Updated SignUp.js and Input.js

--- front-url-shortener/src/components/SignUp/SignUp.js
+++ front-url-shortener/src/components/SignUp/SignUp.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Form from '../layout/Form/Form';
 import Button from '../layout/Button/Button';
 import InputField from '../layout/Input/Input';
 
 const SignUp = () => {
+
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");  
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const user = {
+      pseudo: pseudo,
+      email: email,
+      password: password
+    };
+    
+    console.log(user);
+
+    axios.post(`http://localhost:8201/user/signup`, {user})
+      .then(res => {        
+        console.log(res);
+        console.log(res.data);
+      })    
+  }
+
   return(
     <div>
-      <h1>S'inscrire</h1>
       <Form
         title="S'inscrire"
         subtitle="Hey bienvenue !"
@@ -14,9 +37,12 @@ const SignUp = () => {
         link="/"
         link_text="Connecte-toi !"
       >
-        <InputField label="Pseudo" id="label" type="text" placeholder="Votre pseudo" />
-
-        <Button type="common">Envoyer</Button>
+        <form>
+          <InputField label="pseudo" id="pseudo" type="text" value={pseudo} onChange={e => setPseudo(e.target.value)} placeholder="Votre pseudo" />
+          <InputField label="email" id="email" type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Votre email" />
+          <InputField label="password" id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Votre password" />
+          <Button type="submit" onClick={handleSubmit}>Envoyer</Button>
+        </form>        
       </Form>
     </div>
   )
