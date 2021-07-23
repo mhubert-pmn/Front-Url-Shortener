73b2b2592826205cb86d4ca0c19fc73077e00379
Manon-Seb <manon.sebaoun@free.fr>
2021-07-23 17:47:45 +0200

    [Feature] Update link back to front + data retrieved in the shorten-url__success block

--- front-url-shortener/src/components/ShortenUrl/ShortenUrl.js
+++ front-url-shortener/src/components/ShortenUrl/ShortenUrl.js
@@ -4,24 +4,34 @@ import InputField from '../layout/Input/Input';
 import Button from '../layout/Button/Button';
 import './shortenUrl.scss';
 
+// Decode token to retrieve the id of the current user
+const token = localStorage.getItem("token");
+let token_payload = token !== null ? JSON.parse(atob(token.split('.')[1])) : null;
+let user_idUser = token_payload !== null ? token_payload.id : null;
+
 const ShortenUrl = () => {
   const [originLink, setOriginLink] = useState(null);
   const [isLinkSent, setIsLinkSent] = useState(false);
-
-  /* user_idUser should be retrieved thanks to the token */
-  const user_idUser = 1;
+  const [customLink, setCustomLink] = useState(null);
+  const [showWarning, setShowWarning] = useState(false);
 
   const handleSubmit = () => {
-    axios.post('http://localhost:8201/links', { originLink, user_idUser }).then(
-      response => {
-        const data = response.data;
-        console.log(data);
-        setIsLinkSent(true);
-      },
-      error => {
-        console.log(error);
-      }
-    )
+    if (originLink !== null) {
+      axios.post('http://localhost:8201/links', { originLink, user_idUser }).then(
+        response => {
+          const data = response.data;
+          console.log(data);
+          setCustomLink(data.customLink);
+          setIsLinkSent(true);
+        },
+        error => {
+          console.log(error);
+        }
+      )
+    }
+    else {
+      setShowWarning(true);
+    }
   };
 
   return(
@@ -35,12 +45,13 @@ const ShortenUrl = () => {
         <InputField id="originLink" type="text" placeholder="URL à raccourcir" onChange={e => setOriginLink(e.target.value)}/>
         <Button type="common" onClick={handleSubmit}>Raccourcir</Button>
       </div>
+      {showWarning && <p style={{color: "red", textAlign: "center"}}>Veuillez remplir le champ.</p>}
 
       {isLinkSent && (
         <div className="shorten-url__success">
           <h2>URL raccourcie avec succès !</h2>
           <p><span>URL d'origine</span> : <a href={originLink} target="_blank" rel="noreferrer">{originLink}</a> </p>
-          <p><span>URL raccourcie</span> : </p>
+          <p><span>URL raccourcie</span> : <a href={customLink} target="_blank" rel="noreferrer">{customLink}</a> </p>
         </div>
       )}
     </div>
