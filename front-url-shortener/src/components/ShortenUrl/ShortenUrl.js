172293f94390053b8b27477e92420f62b27f26be
Manon-Seb <manon.sebaoun@free.fr>
2021-07-23 12:08:34 +0200

    [Feature] Create and style ShortenUrl component
    - Update content
    - Add style
    - Fix InputField component style

--- front-url-shortener/src/components/ShortenUrl/ShortenUrl.js
+++ front-url-shortener/src/components/ShortenUrl/ShortenUrl.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import InputField from '../layout/Input/Input';
+import Button from '../layout/Button/Button';
+import './shortenUrl.scss';
 
 const ShortenUrl = () => {
   return(
-    <div>
-      <h1>Raccourcir une URL</h1>
+    <div className="shorten-url">
+      <div className="shorten-url__titles">
+        <h1>Marre des URLs trop longues ?</h1>
+        <h2>Raccourcissez-les en un clic !</h2>
+      </div>
+
+      <div className="shorten-url__form">
+        <InputField id="originLink" placeholder="URL à raccourcir" />
+        <Button type="common">Raccourcir</Button>
+      </div>
+
+      <div className="shorten-url__success">
+        <h2>URL raccourcie avec succès !</h2>
+        <p><span>URL d'origine</span> : </p>
+        <p><span>URL raccourcie</span> : </p>
+      </div>
     </div>
   )
 }
